cbd98ebb524ae79961d80d072d3c3dd47695e359
mkhannouz <martin.khannouz@enseirb-matmeca.fr>
2015-01-25 15:13:20 +0100

    Add saving of each state and action during each turn

--- Hippocampus.cpp
+++ Hippocampus.cpp
@@ -1,16 +1,43 @@
 #include "Hippocampus.hpp"
+#include <sstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "config.hpp"
 #include "State.hpp"
 
+Hippocampus::Hippocampus(bool haveToSaveMemory)
+{
+	haveToSave = haveToSaveMemory;
+	if(haveToSave && ! dirExists(savingLocation))
+		haveToSave = false;
+	if(haveToSave)
+		std::cout << "Saving memory in : " << savingLocation << std::endl;
+}
 void Hippocampus::addState(State& action, State& state)
 {
+	saveState(action, state);
+	++timeCount;
 }
 State* Hippocampus::addState(State& state)
 {
 	//intern boui boui to create random action	
 	State* randAction = new State();
 	randomizeAction(*randAction);
+	saveState(*randAction, state);
+	++timeCount;
 	return randAction;
+}
+void Hippocampus::saveState(State& action, State& state)
+{
+	if(!haveToSave)
+		return;
+	std::stringstream streamAction, streamState;
+	streamAction << savingLocation << timeCount << "_action.json";
+	streamState << savingLocation << timeCount << "_state.json";
+	action.save(streamAction.str(), "plain");
+	state.save(streamState.str(), "plain");
 }
 void Hippocampus::orderState(void)
 {
@@ -18,9 +45,16 @@ void Hippocampus::orderState(void)
 void Hippocampus::randomizeAction(State& action)
 {
 	double const high = 10;
-	double const speed = 10;
+	double speed = 10;
+	double orientation = 5, rotation;
 	int randChoice = rand() %100;
-
+	//std::cout << "randomizeAction " << randChoice << std::endl;
+	//std::cin >> rotation;
+	//std::cin >> orientation;
+	//std::cin >> speed;
+	//action[IDX_ACTION][IDX_BODY][IDX_ROTATION] = rotation;
+	//action[IDX_ACTION][IDX_BODY][IDX_TRANSLATION][IDX_ORIENTATION] = orientation;
+	//action[IDX_ACTION][IDX_BODY][IDX_TRANSLATION][IDX_SPEED] = speed;
 
 	if(randChoice < 10) {//eat something
 		action[IDX_ACTION][IDX_GESTURE][IDX_INGEST] = true;
@@ -39,4 +73,17 @@ void Hippocampus::randomizeAction(State& action)
 	else {//stop everything
 		action[IDX_ACTION][IDX_BODY][IDX_TRANSLATION][IDX_SPEED] = 0;
 	}
+}
+bool Hippocampus::dirExists(std::string const& path)
+{
+	return dirExists(path.c_str());
+}
+bool Hippocampus::dirExists(const char *path)
+{
+	struct stat info;
+	if(stat( path, &info ) != 0)
+		return false;
+	else if(info.st_mode & S_IFDIR)
+		return true;
+	return false;
 }
--- Hippocampus.hpp
+++ Hippocampus.hpp
@@ -1,10 +1,18 @@
 #pragma once
+#include <iostream>
 class State;
 
 class Hippocampus
 {
+	int timeCount = 0;
+	bool haveToSave;
+	std::string savingLocation = "memory/";
 	void randomizeAction(State& action);
+	void saveState(State& action, State& state);
+	bool dirExists(std::string const& path);
+	bool dirExists(const char *path);
 	public:
+		Hippocampus(bool haveToSaveMemory = false);
 		State* addState(State& state);
 		void addState(State& action, State& state);
 		void orderState(void);
