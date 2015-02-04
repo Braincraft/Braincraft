b49fdf788b43149eba5eeac167ee798f6f0bcef0
mkhannouz <martin.khannouz@enseirb-matmeca.fr>
2015-02-04 21:42:38 +0100

    Change the color thing

--- Hippocampus.cpp
+++ Hippocampus.cpp
@@ -45,6 +45,8 @@ State* Hippocampus::addState(State& state)
 }
 void Hippocampus::saveState(State& action, State& state)
 {
+	std::vector<bool> color = colorate(state);
+	gaugeVariation.push_front(color);
 	allMemory.push_front(std::make_pair(&action, &state));
 	if(!haveToSave)
 		return;
@@ -54,25 +56,35 @@ void Hippocampus::saveState(State& action, State& state)
 	action.save(streamAction.str(), "plain");
 	state.save(streamState.str(), "plain");
 }
-void Hippocampus::orderState(void)
-{
-}
 bool Hippocampus::isThereWorryingStuff(State& action, State& state)
 {
 	//Found some coloration
 	//Pour le moment une couleur est une variation négative d'une jauge
-	int color = colorate(state);
-	if(color == no_color)
-		return false;
+	std::list<int> dangerousGauge = getDangerousGauge(state);
 	std::pair<State*, State*> loutre;
-	if(findNearColoredState(color, loutre, state))
+	for(int gauge : dangerousGauge)
 	{
-		fprintf(stderr, "Use a previous state (%d)\n", (int)(*loutre.second)[IDX_NOW][IDX_TIME_STEP]);
-		action = *loutre.first;
-		return true;
+		if(findNearColoredState(gauge, loutre, state))
+		{
+			fprintf(stderr, "Use a previous state (%d)\n", (int)(*loutre.second)[IDX_NOW][IDX_TIME_STEP]);
+			action = *loutre.first;
+			return true;
+		}
 	}
-	
 	return false;
+}
+std::list<int> Hippocampus::getDangerousGauge(State& state)
+{
+	std::list<int> ret;
+	const double limit = 0.7;
+	
+	if((double) state[IDX_SENSATION][IDX_VITAL][IDX_FOOD] < limit)
+		ret.push_front(hungry_color);
+	if((double) state[IDX_SENSATION][IDX_VITAL][IDX_OXYGEN] < limit)
+		ret.push_front(oxygen_color);
+	if((double) state[IDX_SENSATION][IDX_VITAL][IDX_ENERGY] < limit)
+		ret.push_front(energy_color);
+	return ret;
 }
 double Hippocampus::distance(State& s1, State& s2)
 {
@@ -81,57 +93,46 @@ double Hippocampus::distance(State& s1, State& s2)
 bool Hippocampus::findNearColoredState(char color, std::pair<State*, State*>& sameColoredState, State& state)
 {
 	//Parcour la memory pour trouver un instant coloré de color et où l'instant suivant, la couleur n'est plus
-	if(allMemory.size() == 1)
-		return false;
-	auto firstTime = allMemory.begin();
-	auto secondTime = allMemory.begin();
-	auto thirdTime = allMemory.begin();
-	++secondTime;
-	++thirdTime;
-	++thirdTime;
+	auto itAllMemory = allMemory.begin();
+	auto itGaugeVariation = gaugeVariation.begin();
 	std::pair<State*, State*> best;
 	double bestDistance;
 	bool foundBest = false;
-	while(secondTime != allMemory.end())
+	while(itAllMemory != allMemory.end())
 	{
-		State* firstState = firstTime->second;
-		State* secondState = secondTime->second;
-		State* thirdState = thirdTime->second;
-		char firstColor = colorate(*secondState, *firstState);
-		char secondColor = colorate(*thirdState, *secondState);
-		if(secondColor == color && secondColor != firstColor)
+		if(itGaugeVariation->at(color))
 		{
-			double dist = distance(*firstState, state);
+			State* current = itAllMemory->second;
+			double dist = distance(*current, state);
 			//I used < instead of <= because the more recent it is, the best.
 			if(foundBest == false || dist < bestDistance)
 			{
-				best = * firstTime;
+				best = *itAllMemory;
 				bestDistance = dist;
 				foundBest = true;
 			}
-		}	
-		++secondTime;
-		++firstTime;
-		++thirdTime;
+		}
+		++itGaugeVariation;
+		++itAllMemory;
 	}
 	if(foundBest)
 		sameColoredState = best;
 	return foundBest;
 }
-char Hippocampus::colorate(State& state)
+std::vector<bool> Hippocampus::colorate(State& state)
 {
+	if(allMemory.size() == 0)
+		return std::vector<bool>(3, false);//FIXME Magic number
 	State& previous = *(allMemory.front().second);
 	return colorate(previous, state);
 }
-char Hippocampus::colorate(State& previous, State& state)
+std::vector<bool> Hippocampus::colorate(State& previous, State& state)
 {
-	if((double) state[IDX_SENSATION][IDX_VITAL][IDX_FOOD] < (double) previous[IDX_SENSATION][IDX_VITAL][IDX_FOOD])
-		return hungry_color;
-	if((double) state[IDX_SENSATION][IDX_VITAL][IDX_OXYGEN] < (double) previous[IDX_SENSATION][IDX_VITAL][IDX_OXYGEN])
-		return oxygen_color;
-	if((double) state[IDX_SENSATION][IDX_VITAL][IDX_ENERGY] < (double) previous[IDX_SENSATION][IDX_VITAL][IDX_ENERGY])
-		return hungry_color;
-	return no_color;
+	std::vector<bool> ret(3, false);
+	ret[oxygen_color] = ((double) state[IDX_SENSATION][IDX_VITAL][IDX_FOOD] > (double) previous[IDX_SENSATION][IDX_VITAL][IDX_FOOD]);
+	ret[hungry_color] = ((double) state[IDX_SENSATION][IDX_VITAL][IDX_OXYGEN] > (double) previous[IDX_SENSATION][IDX_VITAL][IDX_OXYGEN]);
+	ret[energy_color] = ((double) state[IDX_SENSATION][IDX_VITAL][IDX_ENERGY] > (double) previous[IDX_SENSATION][IDX_VITAL][IDX_ENERGY]);
+	return ret;
 }
 void Hippocampus::randomizeAction(State& action)
 {
@@ -160,7 +161,6 @@ void Hippocampus::randomizeAction(State& action)
 	else if(randChoice <60) {//Jump
 		action[IDX_ACTION][IDX_JUMP] = true;
 	}
-
 	else {//stop everything
 		action[IDX_ACTION][IDX_BODY][IDX_TRANSLATION][IDX_SPEED] = 0;
 	}
--- Hippocampus.hpp
+++ Hippocampus.hpp
@@ -1,17 +1,18 @@
 #pragma once
 #include <iostream>
 #include <list>
+#include <vector>
 class State;
 
 class Hippocampus
 {
-	static const char no_color = 0;
-	static const char oxygen_color = 1;
-	static const char hungry_color = 2;
-	static const char energy_color = 3;
+	static const char oxygen_color = 0;
+	static const char hungry_color = 1;
+	static const char energy_color = 2;
 	int timeCount = 0;
 	bool haveToSave;
 	std::list<std::pair<State*, State*>> allMemory;
+	std::list<std::vector<bool>> gaugeVariation;
 	std::string savingLocation = "memory/";
 	
 	
@@ -20,15 +21,15 @@ class Hippocampus
 	bool dirExists(std::string const& path);
 	bool dirExists(const char *path);
 	bool isThereWorryingStuff(State& action, State& state);
-	char colorate(State& state);
-	char colorate(State& previous, State& state);
+	std::vector<bool> colorate(State& state);
+	std::vector<bool> colorate(State& previous, State& state);
 	bool findNearColoredState(char color, std::pair<State*, State*>& sameColoredState, State& state);
 	double distance(State& s1, State& s2);
+	std::list<int> getDangerousGauge(State& state);
 	public:
 		Hippocampus(bool haveToSaveMemory = false);
 		State* addState(State& state);
 		void addState(State& action, State& state);
-		void orderState(void);
 		void alzheimer(void);
 		~Hippocampus();
 };
